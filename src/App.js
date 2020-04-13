f4851091baa80962a10055a32065fbaec6b56d27
Daniel Cohen <danielcoam@gmail.com>
2020-04-13 10:43:28 +0300

    Add Bootstrap. Fix Routes.js. Remove Tests

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import {Provider} from 'react-redux';
 import configureStore from './configureStore'
 import {HashRouter as Router} from 'react-router-dom';
 import Routes from './Routes'
+
 let store = configureStore();
 
 function App() {
     return (
         <Provider store={store}>
             <Router>
-                <h1>Hello</h1>
                 <Routes/>
             </Router>
         </Provider>
