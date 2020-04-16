5e5c685db4aadf92ca1c9e8c4089eae52c3fd244
Daniel Cohen <danielcoam@gmail.com>
2020-04-16 19:38:23 +0300

    Add `myProperties` to rootReducer.js

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import imagesReducer from './imagesReducer';
+import myPropertiesReducer from "./propertiesReducer";
 
 const rootReducer = combineReducers({
     images: imagesReducer,
+    myProperties: myPropertiesReducer,
 
 });
 export default rootReducer
