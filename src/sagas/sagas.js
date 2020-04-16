3b42588da7639398b40ac8680da56254bc9cc059
Daniel Cohen <danielcoam@gmail.com>
2020-04-16 19:37:00 +0300

    Add propertiesReducer.js, propertyActions.js, propertiesSaga.js

--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 
 import imagesSaga from './imagesSaga';
+import propertiesSaga from "./propertiesSaga";
 
 export default function* rootSaga() {
-    yield all([imagesSaga(),]);
+    yield all([imagesSaga(), propertiesSaga()]);
 }
