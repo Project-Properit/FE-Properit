d94472865eb468bbb0e7c594bc2f2e3c7efe5499
Daniel Cohen <danielcoam@gmail.com>
2020-04-16 19:37:47 +0300

    Refactor properties to use actual data from store

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,37 +1,65 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
+import {loadProperties} from "../actions/propertyActions";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import {PropertyCard} from "./PropertyCard";
 
-class  Properties extends Component{
+class Properties extends Component {
     componentDidMount() {
-        //    Load Something
+        const {userId} = this.props.match.params;
+        this.props.loadProperties(userId);
     }
-    render(){
+
+    render() {
         const {userId} = this.props.match.params;
 
         return (
 
-            <div className="App">
-                <header className="App-header">
-
+            <Container className="App">
+                <header style={{marginBottom: '4rem', textAlign: 'center'}} className="App-header">
                     <h2> Properties - {userId}</h2>
                 </header>
+                {this.props.isLoading &&
+                <div>Loading..</div>
+                }
+                <Row>
+                    {this.props.myProperties.map(prop => (
+                            <PropertyCard
+                                onRemove={(propId)=>this.onRemoveProperty(propId)}
+                                infoUrl={this.onInfoProperty(prop.prop_id)}
+                                key={prop.prop_id}
+                                property={prop}/>
+                    ))}
+                </Row>
+                <div>
+                    {this.props.error}
+                </div>
 
-            </div>
+            </Container>
         )
+    }
+
+    onRemoveProperty(propId) {
+        console.log('Remove ' +propId);
+    }
+
+    onInfoProperty(propId) {
+        return this.props.match.url +'/' +propId
     }
 }
 
-// const mapStateToProps = ({ isLoading, images, error }) => ({
-//     isLoading,
-//     images,
-//     error,
-// });
+const mapStateToProps = ({myProperties}) => ({
+    isLoading: myProperties.isLoading,
+    myProperties: myProperties.myProperties,
+    error: myProperties.error,
+});
 
-// const mapDispatchToProps = dispatch => ({
-//     loadImages: () => dispatch(loadImages()),
-// });
+const mapDispatchToProps = dispatch => ({
+    loadProperties: (userId) => dispatch(loadProperties(userId)),
+});
 
 export default connect(
-    null,
-    null,
+    mapStateToProps,
+    mapDispatchToProps,
 )(Properties);
