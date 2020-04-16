3bf483c752de2a4aaedf45d333b4187ddf81168b
Daniel Cohen <danielcoam@gmail.com>
2020-04-16 19:38:07 +0300

    Add `fetchProperties` to api.js

--- src/api.js
+++ src/api.js
@@ -13,4 +13,21 @@ const fetchImages = () => {
     // }
     // return data;
 };
-export {fetchImages,}
+const fetchProperties = async (userId) => {
+     return  new Promise(resolve => {
+        setTimeout(() => resolve([
+            {prop_id:1, tenants:["ten1", "ten2"], name: "First Prop", address: "Address 1"},
+            {prop_id:2, tenants:["ten1", "ten2"], name: "Second Prop", address: "Address 2"},
+            {prop_id:3, tenants:["ten1", "ten2"], name: "Second Prop", address: "Address 2"},
+            {prop_id:4, tenants:["ten1", "ten2"], name: "Second Prop", address: "Address 2"},
+        ]), TIMEOUT)
+    })
+    // throw new Error("dsdsdsds");
+    // const response = await fetch("http://fdfdsfdsfdsfdsfd");
+    // const data = await response.json();
+    // if (response.status >= 400) {
+    //     throw new Error(data.errors);
+    // }
+    // return data;
+};
+export {fetchImages,fetchProperties}
