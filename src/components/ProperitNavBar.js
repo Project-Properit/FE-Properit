9cc189e6c2a328e639ce800ee683cfca4df62c3e
arviv <arvivro@gmail.com>
2020-06-09 00:05:43 +0300

    add is owner and is tenant

--- src/components/ProperitNavBar.js
+++ src/components/ProperitNavBar.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import logoutAction from "../actions/logoutActions";
+import {logoutAction}from "../actions/logoutActions";
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import logo from "../images/logoWhite .jpg";
@@ -9,19 +9,21 @@ import logo from "../images/logoWhite .jpg";
 class ProperitNavBar extends Component {
     render() {
         const isLogin = this.props.token
-        // if(href == "/"){
-        //      return (null)
-        // }
+        const isOwner = this.props.isOwner
+        const isTenant = this.props.isTenant
         if(isLogin) {
-
-
             return (
 
                 <Navbar bg="dark" variant="dark">
                     <Navbar.Brand href="/properties"><img className="logo" style={{height: '60px'}} src={logo}
                                                           alt='logo'/></Navbar.Brand>
                     <Nav className="mr-auto">
-                        {isLogin &&
+                        {isOwner && isTenant &&
+                        <>
+                            <Nav.Link onClick={()=> logoutAction()}>Logout</Nav.Link>
+                        </>
+                        }
+                        {isLogin && (!isOwner || !isTenant) &&
                         <>
                             <Nav.Link href="/properties">My Properties</Nav.Link>
                             <Nav.Link href={`/renters`}>My Renters</Nav.Link>
@@ -30,14 +32,7 @@ class ProperitNavBar extends Component {
                             <Nav.Link href='/about'>About</Nav.Link>
                             <Nav.Link href="/documents">Document</Nav.Link>
                             <Nav.Link onClick={() => this.props.logout()}>Logout</Nav.Link>
-
                         </>}
-                        {/*{!isLogin &&*/}
-                        {/*<>*/}
-                        {/*    <Nav.Link href="/login">Login</Nav.Link>*/}
-                        {/*    <Nav.Link href="/signup">Register</Nav.Link>*/}
-                        {/*</>*/}
-                        {/*}*/}
                     </Nav>
                 </Navbar>
             );
@@ -52,6 +47,8 @@ const mapDispatchToProps = dispatch => ({
 const mapStateToProps = ({clientReducer}) => ({
     token: clientReducer.token,
     userId: clientReducer.userId,
+    isOwner: clientReducer.isOwner,
+    isTenant: clientReducer.isTenant,
 });
 export default connect(
     mapStateToProps,
