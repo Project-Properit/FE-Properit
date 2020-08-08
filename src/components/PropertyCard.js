d435825378879f7061b4f92845911ae0a34ffc05
shoham <shohamyaakov115@gmail.com>
2020-08-08 19:50:04 +0300

    ari

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -6,38 +6,46 @@ import home2 from '../images/home2.jpg'
 import {Button, IconButton } from "@material-ui/core";
 import {Add, Edit, OpenInBrowser} from "@material-ui/icons";
 import Tooltip from '@material-ui/core/Tooltip';
+import HomeIcon from '../images/homeCard.png'
 
 export const PropertyCard = props => {
 	const prop = props.property;
-	const propImage = prop.img_url ? prop.img_url : 'https://properit.s3.amazonaws.com/house1.jpg'
+	// const propImage = prop.img_url ? prop.img_url : 'https://properit.s3.amazonaws.com/house1.jpg'
 
 	return (
 		<div className="propBody">
 			<div className="propCard">
 				<div className="imgBox">
-					<h2> {prop.address}</h2>
-					<img className="propImg" src="https://properit.s3.amazonaws.com/house1.jpg"/>
+					<img src={HomeIcon}/>
 				</div>
-				{/*<div className="details">*/}
-				<div className="propCard-text">
-					{/*<span className="tenants">{prop.tenant_list.length || 0} tenants</span>*/}
-					<span>{prop.tenant_list.length || 0} tenants</span>
+				<div className="contentBx">
 					<h2> {prop.address}</h2>
-					<p>{prop.comments}</p>
-					<Tooltip title="Edit The Property" placement="right-start">
-						<Card.Link as={Link} to={props.editUrl} className="EditValue"><Edit/></Card.Link>
-					</Tooltip>
-					{/*<div className="propCard-stats">*/}
-					{/*	<div className="stat">*/}
-					{/*		<Card.Link as={Link} to={props.infoUrl} className="ChooseValue"><OpenInBrowser/> Choose</Card.Link>*/}
-					{/*	</div>*/}
-					{/*</div>*/}
-					<Button variant="outlined" color="primary" onClick={()=>props.onChoose()}>
-						<Card.Link as={Link} to={props.infoUrl} className="ChooseValue"><OpenInBrowser/> Choose</Card.Link>
-					</Button>
-				</div>
-			</div>
+					<div className="propCard-text">
+						<span>{prop.tenant_list.length || 0} tenants</span>
+						<p>{prop.comments}</p>
+						<Tooltip title="Edit The Property" placement="right-start">
+									{/*<Card.Link as={Link} to={props.editUrl} className="EditValue"><Edit/></Card.Link>*/}
+							<a href={props.editUrl} className="EditValue"><Edit/></a>
+						</Tooltip>
+					</div>
+					<a href={props.infoUrl} className="Choose">Choose</a>
 
+				</div>
+				{/*	<h2> {prop.address}</h2>*/}
+				{/*	<img className="propImg" src="https://properit.s3.amazonaws.com/house1.jpg"/>*/}
+				{/*</div>*/}
+				{/*<div className="propCard-text">*/}
+				{/*	<span>{prop.tenant_list.length || 0} tenants</span>*/}
+				{/*	<h2> {prop.address}</h2>*/}
+				{/*	<p>{prop.comments}</p>*/}
+				{/*	<Tooltip title="Edit The Property" placement="right-start">*/}
+				{/*		<Card.Link as={Link} to={props.editUrl} className="EditValue"><Edit/></Card.Link>*/}
+				{/*	</Tooltip>*/}
+				{/*	<Button variant="outlined" color="primary" onClick={()=>props.onChoose()}>*/}
+				{/*		<Card.Link as={Link} to={props.infoUrl} className="ChooseValue"><OpenInBrowser/> Choose</Card.Link>*/}
+				{/*	</Button>*/}
+				{/*</div>*/}
+			</div>
 		</div>
 	)
 };
